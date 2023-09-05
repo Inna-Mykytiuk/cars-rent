3085ef2a88cffc566b44eeb0468a6ff3aebc4766
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-09-05 19:26:34 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/cars-rent@f8deb69282adc43e1c43fbad3d3e57b6d8ea1b51 🚀

---
+++ static/js/617.a2d58310.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcars_rent=self.webpackChunkcars_rent||[]).push([[617],{8617:function(r,t,n){n.d(t,{wkn:function(){return a}});var e=n(9983);function a(r){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"}}]})(r)}}}]);
+//# sourceMappingURL=617.a2d58310.chunk.js.map
