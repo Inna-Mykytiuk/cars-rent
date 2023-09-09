c65ace60ef94429bbe2bc0a7fe0e7ec83caaab4c
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-09 16:39:24 +0300

    change loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,147 @@
-import React from 'react';
-import { ThreeDots } from 'react-loader-spinner';
-import { LoaderContainer } from './Loader.styled';
+import React, {useState, useEffect, useCallback} from 'react';
+// import { ThreeDots } from 'react-loader-spinner';
 
-const Loader = () => {
+// У файлі Loader.js
+import { LoaderContainer, Rect, Container } from './Loader.styled';
+
+const TIMER = 150; // Milliseconds between moving the next block
+const TRANSITION = 0.5; // Seconds to actually move one block
+const DEF_SIZE = 60; // Pixels height/width
+const GUTTER = 5; // Spacing in percentage between tiles
+
+const initialState = {
+  positions: {
+    1: 'alpha',
+    2: 'bravo',
+    3: 'charlie',
+    4: null,
+    5: 'delta',
+    6: 'echo',
+    7: 'foxtrot',
+  },
+  stateNumber: 0,
+};
+
+const Loader = ({ size, style, center }) => {
+  const [state, setState] = useState(initialState);
+
+  const positionForTile = useCallback((radioCommand) => {
+    for (const position in state.positions) {
+      const tile = state.positions[position];
+      if (tile === radioCommand) {
+        return position;
+      }
+    }
+  }, [state]);
+
+  const tileIndexToMove = useCallback(() => {
+    switch (state.stateNumber) {
+      case 0:
+        return 7;
+      case 1:
+        return 6;
+      case 2:
+        return 5;
+      case 3:
+        return 4;
+      case 4:
+        return 3;
+      case 5:
+        return 2;
+      case 6:
+        return 1;
+      case 7:
+        return 4;
+      default:
+        return 0;
+    }
+  }, [state]);
+
+  const setNextState = useCallback(() => {
+    const currentPositions = state.positions;
+    const emptyIndex = positionForTile(null);
+    const indexToMove = tileIndexToMove();
+    const newPositions = {
+      ...currentPositions,
+      [indexToMove]: null,
+      [emptyIndex]: currentPositions[indexToMove],
+    };
+
+    const currentState = state.stateNumber;
+    const nextState = currentState === 7 ? 0 : currentState + 1;
+
+    setState({ stateNumber: nextState, positions: newPositions });
+  }, [state, positionForTile, tileIndexToMove]);
+
+  useEffect(() => {
+    const timerId = setInterval(setNextState, TIMER);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [setNextState]);
+
+  const clipPathForPosition = (position) => {
+    position = parseInt(position, 10);
+    const SIZE = (100 - 2 * GUTTER) / 3;
+    const VAR0 = '0% ';
+    const VAR1 = SIZE + GUTTER + '% ';
+    const VAR2 = 2 * SIZE + 2 * GUTTER + '% ';
+    switch (position) {
+      case 1:
+        return `inset(${VAR1}${VAR2}${VAR1}${VAR0}round 5%)`;
+      case 2:
+        return `inset(${VAR0}${VAR2}${VAR2}${VAR0}round 5%)`;
+      case 3:
+        return `inset(${VAR0}${VAR1}${VAR2}${VAR1}round 5%)`;
+      case 4:
+        return `inset(${VAR1}${VAR1}${VAR1}${VAR1}round 5%)`;
+      case 5:
+        return `inset(${VAR2}${VAR1}${VAR0}${VAR1}round 5%)`;
+      case 6:
+        return `inset(${VAR2}${VAR0}${VAR0}${VAR2}round 5%)`;
+      case 7:
+        return `inset(${VAR1}${VAR0}${VAR1}${VAR2}round 5%)`;
+      default:
+        return '';
+    }
+  };
+
+  const styles = {
+    width: DEF_SIZE + 'px',
+    height: DEF_SIZE + 'px',
+    ...style,
+  };
+
+  if (size) {
+    styles.width = size + 'px';
+    styles.height = size + 'px';
+  }
+
+  let className = 'sw-loader__wrapper';
+  if (center) {
+    className += ' sw-loader__wrapper--center';
+  }
+
   return (
-    <LoaderContainer>
-      <ThreeDots
-        height="80"
-        width="80"
-        radius="9"
-        color="#3470ff"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-        visible
-      />
+    <LoaderContainer style={styles} className={className}>
+      <Container>
+        {['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot'].map(
+          (radioCommand) => {
+            const pos = positionForTile(radioCommand);
+            return (
+              <Rect
+                key={'rect-' + radioCommand}
+                clipPath={clipPathForPosition(pos)}
+                transition={`${TRANSITION}s cubic-bezier(0.86, 0, 0.07, 1)`}
+              />
+            );
+          }
+        )}
+      </Container>
     </LoaderContainer>
   );
 };
 
 export default Loader;
+
--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,9 +1,54 @@
 import styled from '@emotion/styled';
 
 export const LoaderContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0.7;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  ${({ center }) =>
+    center &&
+    `
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  `}
 `;
+
+export const Container = styled.div`
+  width: 70%;
+  height: 70%;
+  margin-left: 15%;
+  margin-top: 15%;
+  position: relative;
+  transform: rotate(-45deg);
+`;
+
+export const Rect = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: linear-gradient(
+    to right,
+    #141562,
+    #486fbc,
+    #eab5a1,
+    #8dd6ff,
+    #4973c9,
+    #d07ca7,
+    #f4915e,
+    #f5919e,
+    #b46f89,
+    #141562,
+    #486fbc
+  );
+  background-position: 0% 50%;
+  background-size: 1000% 1000%;
+  overflow: hidden;
+  animation: moveGradient 15s infinite;
+  ${({ clipPath }) => `clip-path: ${clipPath};`}
+  ${({ transition }) => `transition: ${transition};`}
+`;
+
+
