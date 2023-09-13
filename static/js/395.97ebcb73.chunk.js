3a8f26498133589381b473a915c6db087fabc55c
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-09-13 19:30:30 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/cars-rent@64f8ea9ac99be1e8ae39979edbb8a88971e8187a 🚀

---
+++ static/js/395.97ebcb73.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcars_rent=self.webpackChunkcars_rent||[]).push([[395],{7395:function(n,r,e){e.r(r);var t=e(2178),a=(e(2791),e(5048)),u=e(5834),c=e(184);r.default=function(n){var r=n.data,e=(0,a.v9)((function(n){return n.favorite})),i=r.filter((function(n){return e.includes(n.id)}));return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(u.Z,{children:(0,c.jsx)(t.O,{data:i})})})}}}]);
+//# sourceMappingURL=395.97ebcb73.chunk.js.map
