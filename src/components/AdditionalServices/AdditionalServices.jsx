c9237be7038033c56e42ba1314a97718ca80aa7f
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-14 17:47:45 +0300

    add and styled services cards

--- src/components/AdditionalServices/AdditionalServices.jsx
+++ src/components/AdditionalServices/AdditionalServices.jsx
@@ -1,44 +1,104 @@
 import React from 'react';
+
 import {
   ServicesSection,
-  Card,
+  ServiceCard,
   Face1,
   Face2,
-  Content,
+  ServiceContent,
   // Image,
   Title,
   Paragraph,
+  ServiceCardWrapper,
+  ServiceImg
 } from './AdditionalServices.styled';
 import Container from 'components/Container/Container';
 // import services from 'images/services.png';
+// import services from '../helpers/services.json';
+import towing from 'images/serv/towing.svg'
+import fuel from 'images/serv/speed.svg'
+import tire from 'images/serv/tires.svg'
+import washing from 'images/serv/wash.svg'
+import mechanic from 'images/serv/mechanic.svg'
 
 const AdditionalServices = () => {
   return (
     <ServicesSection>
       <Container>
-        {/* <ServicesImg src={services} alt="services"  width='300px'/> */}
-        <Card>
-          <Face1>
-            <Content>
-              {/* <Image
-                src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"
-                alt="Design"
-              /> */}
-              <Title>Design</Title>
-            </Content>
-          </Face1>
-          <Face2>
-            <Content>
-              <Paragraph>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                cum cumque minus iste veritatis provident at.
-              </Paragraph>
-            </Content>
-          </Face2>
-        </Card>
+        <ServiceCardWrapper>
+
+            <ServiceCard >
+              <Face1>
+                <ServiceContent>
+                <ServiceImg src={fuel} alt='Fuel Delivery' width={50} height={50} fill="#FF0000"/>
+
+                </ServiceContent>
+              </Face1>
+              <Face2>
+                <ServiceContent>
+                  <Title>Fuel Delivery</Title>
+                  <Paragraph>Order a fuel delivery, and we'll supply the required amount of fuel directly to your vehicle.</Paragraph>
+                </ServiceContent>
+              </Face2>
+            </ServiceCard>
+            <ServiceCard >
+              <Face1>
+                <ServiceContent>
+                <ServiceImg src={towing} alt='Towing'  width={50} height={50} />
+                </ServiceContent>
+              </Face1>
+              <Face2>
+                <ServiceContent>
+                  <Title>Towing</Title>
+                  <Paragraph>Our towing service is ready to transport your vehicle to the nearest service center.</Paragraph>
+                </ServiceContent>
+              </Face2>
+            </ServiceCard>
+            <ServiceCard >
+              <Face1>
+                <ServiceContent>
+                <ServiceImg src={tire} alt='Tire Service'  width={50} height={50}/>
+                </ServiceContent>
+              </Face1>
+              <Face2>
+                <ServiceContent>
+                  <Title>Tire Service</Title>
+                  <Paragraph>Our professional technicians will come to your tire-related issues.</Paragraph>
+                </ServiceContent>
+              </Face2>
+            </ServiceCard>
+            <ServiceCard >
+              <Face1>
+                <ServiceContent>
+                <ServiceImg src={washing} alt='Car Washing'  width={50} height={50}/>
+                </ServiceContent>
+              </Face1>
+              <Face2>
+                <ServiceContent>
+                  <Title>Car Washing</Title>
+                  <Paragraph>Our professional car washers will ensure your car is shining.</Paragraph>
+                </ServiceContent>
+              </Face2>
+            </ServiceCard>
+            <ServiceCard >
+              <Face1>
+                <ServiceContent>
+                <ServiceImg src={mechanic} alt='Mobile Mechanic' width={50} height={50} />
+                </ServiceContent>
+              </Face1>
+              <Face2>
+                <ServiceContent>
+                  <Title>Mobile Mechanic</Title>
+                  <Paragraph>Our qualified mobile mechanics will fix the issue promptly.</Paragraph>
+                </ServiceContent>
+              </Face2>
+            </ServiceCard>
+
+        </ServiceCardWrapper>
       </Container>
     </ServicesSection>
   );
 };
 
 export default AdditionalServices;
+
--- src/components/AdditionalServices/AdditionalServices.styled.jsx
+++ src/components/AdditionalServices/AdditionalServices.styled.jsx
@@ -1,11 +1,11 @@
-import styled from "@emotion/styled/macro";
+import styled from '@emotion/styled/macro';
 
 export const ServicesSection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 30px 0;
-  padding: 10px 0;
+  padding: 30px 0;
   background: #657396;
 
   background-repeat: no-repeat;
@@ -13,8 +13,8 @@ export const ServicesSection = styled.div`
   background-size: cover;
   background-position: center;
   width: 100%;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 5px, rgba(0, 0, 0, 0.1) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
-
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 8px 5px,
+    rgba(0, 0, 0, 0.1) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
 `;
 
 export const ServicesImg = styled.img`
@@ -34,17 +34,26 @@ export const ServicesImg = styled.img`
   }
 `;
 
-
 export const Face1 = styled.div`
   position: relative;
   background: #333;
+  background: #445c84;
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1;
   transform: translateY(0);
-  transition: 3.5s;
-  height: 150px;
+  transition: 1s;
+  height: 80px;
+  border-radius: 14px;
+  @media (min-width: 768px) {
+    width: 100px;
+    height: 100px;
+  }
+  @media (min-width: 1440px) {
+    width: 150px;
+    height: 150px;
+  }
 `;
 
 export const Face2 = styled.div`
@@ -56,72 +65,159 @@ export const Face2 = styled.div`
   padding: 20px;
   box-sizing: border-box;
   box-shadow: 0 20px 50px rgba(0, 0, 0, 0.8);
-  transform: translateY(-150px);
-  transition: 3.5s;
-  height: 150px;
-`;
-export const Content = styled.div`
-  opacity: 0.2;
-  transition: 0.5s;
-`;
+  transform: translateY(-80px);
+  transition: 1s;
+  height: 80px;
+  border-radius: 16px;
 
-export const Card = styled.div`
-  position: relative;
-  cursor: pointer;
-  width: 150px;
-  height: 150px;
-  transition: 0.5s;
-  background: #333;
-  transform: translateY(0);
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  ${Content} {
-    opacity: 0.2;
-    transition: 3.5s;
+  @media (min-width: 768px) {
+    transform: translateY(-100px);
+    height: 100px;
   }
 
+  @media (min-width: 1440px) {
+    transform: translateY(-150px);
+    height: 150px;
+  }
+`;
+
+export const ServiceContent = styled.div`
+  opacity: 0.4;
+  transition: 0.5s;
+`;
+
+export const ServiceCardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const ServiceCard = styled.div`
+  position: relative;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  transition: 0.5s;
+  background: #333;
+  border-radius: 14px;
+  transform: translateY(0);
+
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  ${ServiceContent} {
+    opacity: 0.4;
+    transition: 1s;
+  }
 
   &:hover {
     ${Face1} {
       background: #ff0057;
-      transform: translateY(-75px);
-      transition: 3.5s;
+      transform: translateY(-40px);
+      transition: 1s;
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
 
-      ${Content} {
+      ${ServiceContent} {
         opacity: 1;
       }
     }
 
     ${Face2} {
-      transform: translateY(-75px);
-      transition: 3.5s;
+      transform: translateY(-40px);
+      transition: 1s;
+      border-bottom-left-radius: 14px;
+      border-bottom-right-radius: 14px;
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+    }
+  }
+  @media (min-width: 768px) {
+    width: 100px;
+    height: 100px;
+    &:hover {
+      ${Face1} {
+        background: #ff0057;
+        transform: translateY(-50px);
+        transition: 1s;
+
+        ${ServiceContent} {
+          opacity: 1;
+        }
+      }
+
+      ${Face2} {
+        transform: translateY(-50px);
+        transition: 1s;
+      }
+    }
+  }
+  @media (min-width: 1440px) {
+    width: 150px;
+    height: 150px;
+    &:hover {
+      ${Face1} {
+        background: #ff0057;
+        transform: translateY(-75px);
+        transition: 1s;
+
+        ${ServiceContent} {
+          opacity: 1;
+        }
+      }
+
+      ${Face2} {
+        transform: translateY(-75px);
+        transition: 1s;
+      }
     }
   }
 `;
 
-
-export const Image = styled.img`
-  max-width: 100px;
+export const ServiceImg = styled.img`
+  width: 50px;
+  height: 50px;
+  display: block;
+  margin: 0;
+  padding: 0;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  @media (min-width: 768px) {
+    width: 70px;
+    height: 70px;
+  }
+  @media (min-width: 1440px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 export const Title = styled.h3`
-  margin: 10px 0 0;
+  margin-bottom: 5px;
   padding: 0;
-  color: #fff;
+  color: #000;
   text-align: center;
-  font-size: 1.5em;
+  font-size: 14px;
+  font-family: 'Manrope';
+  @media (min-width: 768px) {
+    margin-bottom: 10px;
+    font-size: 16px;
+  }
+  @media (min-width: 1440px) {
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
 `;
 
 export const Paragraph = styled.p`
   margin: 0;
   padding: 0;
+  display: none;
+  @media (min-width: 1440px) {
+    display: block;
+    font-size: 14px;
+  }
 `;
-
-
-
-
-
-
-
-
-
